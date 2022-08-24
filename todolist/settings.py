01601cedd313f36cc5cae6e71f1e98636901cee4
Libera999 <plamenniy.krug@gmail.com>
2022-08-24 22:10:00 +0300

    d3

--- todolist/settings.py
+++ todolist/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'HT2QTFFrNoerERVWnCvy',
+        'HOST': 'containers-us-west-52.railway.app',
+        'PORT': '5985',
     }
 }
 
